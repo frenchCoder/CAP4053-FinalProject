0f4f58ca5a9ccc31b7c6d85590eb11addc3cac04
frenchCoder <jessica.bourgade@knights.ucf.edu>
2015-03-19 15:48:10 -0400

    script skeletons
    Created object properties and empty methods in previously empty.  Added
    comments and TODOs in the scripts.

---
+++ PirateGame/Assets/Scripts/Ship.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Ship : MonoBehaviour {
+
+	public float maxSpeed;
+	public float minSpeed;
+	public float curSpeed;
+	public float turnSpeed;
+	//amount of gold looted per tick
+	public float lootingSpeed;
+
+	//gold player has in their harbor
+	public float goldInHarbor;
+	//gold player is carrying on their ship
+	public float goldInShip;
+	//gold player has overall
+	public float goldTotal;
+	//max amount of gold a ship can carry
+	public float maxGold;
+
+	//cannons on each side of ship, 3 per side
+	public List<Cannon> leftCannons;
+	public List<Cannon> rightCannons;
+
+	public float points;
+	public float health;
+	public State state;
+
+	//states a ship can be in at any time
+	public enum State
+	{
+		Looting,
+		Shopping,//in harbor
+		Roaming
+	};
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		//TODO: init variables
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//check state and call function to perform action based on that state
+		if (state == State.Looting)
+		{
+			//TODO: call loot()
+		}
+		else if (state == State.Shopping)
+		{
+			//TODO: TBD
+		}
+		else if (state == State.Roaming)
+		{
+			//handled by user or AI object
+		}
+	}
+
+	//attack by shooting cannons from side of ship
+	//"L" for left, "R" for right
+	public void attack (string side)
+	{
+		//TODO: add proper functionality
+	}
+
+	//increase gold on ship each tick
+	public void loot ()
+	{
+		//TODO: add proper functionality
+		//if max gold is reached then return to free roam state
+	}
+
+	void OnTriggerEnter(Collider hit)
+	{
+		print ("hit a trigger");		
+
+		/*TODO: oncollision for cannonhits, 
+			if state is not roaming, then cant be hit
+			health -= cannon.attackpower
+			if heath=0 then ship returns to harbor, enemy ship will receive its gold
+			destroy cannon(here or have cannon object handle it)
+		*/
+	}
+
+	//increase a ship's property
+	public void upgrade ()
+	{
+		//TODO: add functionality
+	}
+
+	//return true if ship is within range on left side
+	public bool enemyShipOnLeft(int range)
+	{
+		//TODO: add functionality, use adj/pie sensor
+	}
+
+	//return true if ship is within range on right side
+	public bool enemyShipOnRight(int range)
+	{
+		//TODO: add functionality, use adj/pie sensor
+	}
+
+}
--- PirateGame/Assets/Scripts/UserShipAgent.cs
+++ PirateGame/Assets/Scripts/UserShipAgent.cs
@@ -2,14 +2,75 @@
 using System.Collections;
 
 public class UserShipAgent : MonoBehaviour {
+	
+	public Ship ship;
+	public Transform harbor;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		//TODO: init variables
+		/*
+			spawn user in their harbor
+			give user enough gold to purchase only 1 upgrade
+			open shop menu for user to purchase item, game timer starts when menu is exited
+		 */
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		//move/attack according to user if roaming
+		if (ship.state == Ship.State.Roaming)
+		{
+			transform.Rotate(Vector3.forward * -Input.GetAxis("Horizontal") * ship.turnSpeed * Time.deltaTime);
+			transform.position += transform.up * ship.curSpeed * Time.deltaTime;
+			
+			ship.curSpeed += Input.GetAxis("Vertical") * Time.deltaTime;
+			ship.curSpeed = Mathf.Clamp(ship.curSpeed, 0.5f, ship.maxSpeed);
+
+			//TODO:add keylisteners for attacking
+			//'Q' shoot left cannons
+			//'E' shoot right rannons
+		}
+		else if (ship.state == Ship.State.Looting)
+		{
+			//TODO
+			/*
+				display text “press space to stop looting”
+				ship object handles looting
+			*/
+		}
+		else if (ship.state == Ship.State.Shopping)
+		{
+			//TODO
+			/*
+				display text “press space to raise anchor”
+				automatically unload gold off ship to harbor
+				display shop menu
+			*/
+		}
 	}
+
+	void OnTriggerEnter(Collider hit)
+	{
+		print("hit a trigger");
+
+		/*TODO: island looting trigger
+			if ship not in looting state
+				display text “press space to start looting”
+			if "space" 
+				looting state => roaming
+				roaming state => looting
+		*/
+			
+		/*TODO: harbor shopping trigger
+			if ship not in shopping state
+				display text “press space to drop anchor”
+			if "space" 
+				shopping state => roaming
+				roaming state => shopping
+	 	*/
+	}
+
 }
