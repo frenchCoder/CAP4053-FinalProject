cd681f5a891b17d80446f974ff7a69e37d6250ed
frenchCoder <jessica.bourgade@knights.ucf.edu>
2015-03-24 21:01:18 -0400

    moveagent fix, library delete

--- PirateGame/Assets/Scripts/UserShipAgent.cs
+++ PirateGame/Assets/Scripts/UserShipAgent.cs
@@ -5,6 +5,7 @@ public class UserShipAgent : MonoBehaviour {
 	
 	public Ship ship;
 	public Transform harbor;
+	private string screenText;
 
 	// Use this for initialization
 	void Start () 
@@ -13,8 +14,10 @@ public class UserShipAgent : MonoBehaviour {
 		/*
 			spawn user in their harbor
 			give user enough gold to purchase only 1 upgrade
-			open shop menu for user to purchase item, game timer starts when menu is exited
+			done in update: open shop menu for user to purchase item, game timer starts when menu is exited
 		 */
+
+		screenText = "";
 	}
 	
 	// Update is called once per frame
@@ -23,10 +26,10 @@ public class UserShipAgent : MonoBehaviour {
 		//move/attack according to user if roaming
 		if (ship.state == Ship.State.Roaming)
 		{
-			transform.Rotate(Vector3.forward * -Input.GetAxis("Horizontal") * ship.turnSpeed * Time.deltaTime);
-			transform.position += transform.up * ship.curSpeed * Time.deltaTime;
+			transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * ship.turnSpeed * Time.deltaTime);			
+			transform.position -= transform.forward * ship.curSpeed * Time.deltaTime;
 			
-			ship.curSpeed += Input.GetAxis("Vertical") * Time.deltaTime;
+			ship.curSpeed += Input.GetAxis("Vertical") * Time.deltaTime;			
 			ship.curSpeed = Mathf.Clamp(ship.curSpeed, 0.5f, ship.maxSpeed);
 
 			//TODO:add keylisteners for attacking
@@ -56,21 +59,38 @@ public class UserShipAgent : MonoBehaviour {
 	{
 		print("hit a trigger");
 
-		/*TODO: island looting trigger
+		if (hit.tag.Equals("island"))
+		{
+			/*TODO: island looting trigger
 			if ship not in looting state
 				display text “press space to start looting”
 			if "space" 
 				looting state => roaming
 				roaming state => looting
-		*/
+			*/
+		}
 			
-		/*TODO: harbor shopping trigger
+
+		else if (hit.name.Equals("Harbor"))
+		{
+			/*TODO: harbor shopping trigger
 			if ship not in shopping state
 				display text “press space to drop anchor”
 			if "space" 
 				shopping state => roaming
 				roaming state => shopping
-	 	*/
+	 		*/
+		}
+	}
+
+	void OnTriggerExit(Collider hit)
+	{
+		screenText = "";
+	}
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect (0,0,190,800), screenText);
 	}
 
 }
