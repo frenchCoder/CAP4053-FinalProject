b1952f2a6402bbb4809d2abe325cdc553e44d05a
frenchCoder <jessica.bourgade@knights.ucf.edu>
2015-04-18 19:14:52 -0400

    new scenes and scaled GUI
    created new scenes, with working buttons to switch scenes.  All that
    remains is to implement the timer.  Fixed all GUIs so that they scale
    with the screen size.

---
+++ PirateGame/Assets/Scripts/StartSceneScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartSceneScript : MonoBehaviour {
+
+	public void playGame() {
+		Debug.Log ("start game");
+		Application.LoadLevel("main");
+	}
+
+	public void viewHowTo() {
+		Debug.Log ("how to play");
+		Application.LoadLevel("howToPage");
+	}
+}
